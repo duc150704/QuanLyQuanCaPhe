de36c33bd46123d12b11159bcbcbf031e4c428fd
QunaQuna <nguyenhoangquan13032004@gmail.com>
2024-11-02 18:12:10 +0700

    Add files via upload

--- QuanLyQuanCafe/DAO/CategoryDAO.cs
+++ QuanLyQuanCafe/DAO/CategoryDAO.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace QuanLyQuanCAFE.DAO
@@ -20,7 +21,7 @@ namespace QuanLyQuanCAFE.DAO
                     instance = new CategoryDAO();
                 return CategoryDAO.instance;
             }
-            private set => CategoryDAO.instance = value; 
+            private set { CategoryDAO.instance = value; }
         }
 
         private CategoryDAO() { }
@@ -38,10 +39,50 @@ namespace QuanLyQuanCAFE.DAO
                 list.Add(category);
 
             }
-
-
-
             return list;
         }
+        public Category GetCategoryByID(int id)
+        {
+            Category category = null;
+
+            string query = "select * from FoodCategory where id = " + id;
+
+            DataTable data = DataProvider.Instance.ExcuteQuery(query);
+
+            foreach (DataRow item in data.Rows)
+            {
+                category = new Category(item);
+                return category;
+            }
+
+            return category;
+        }
+
+        public bool InsertCategory(string name)
+        {
+            string query = string.Format("INSERT dbo.FoodCategory ( name)VALUES  ( N'{0}')", name);
+            int result = DataProvider.Instance.ExcuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool UpdateCategory( string name)
+        {
+            string query = string.Format("UPDATE dbo.FoodCategory SET name = N'{0}' WHERE id = {3}", name);
+            int result = DataProvider.Instance.ExcuteNonQuery(query);
+
+            return result > 0;
+        }
+
+        public bool DeleteCategory(string name)
+        {
+            BillInfoDAO.Instance.DeleteBillInfoByCategoryID(name);
+
+            string query = string.Format("Delete Food where id = {0}", name);
+            int result = DataProvider.Instance.ExcuteNonQuery(query);
+
+            return result > 0;
+        }
     }
+
 }
